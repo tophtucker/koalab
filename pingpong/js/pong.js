50d1c19177791c52c9530ac072cc7ecacd9fef47
samgqroberts <samgqroberts@gmail.com>
2013-05-30 17:20:05 -0400

    okay got pingpong freal

--- pingpong/js/pong.js
+++ pingpong/js/pong.js
@@ -1,35 +1,87 @@
+function populatePlayerSelect(playerChanged) {
+	var p1value, p2value;
+	if (playerChanged > 0) {
+		var p1value = document.getElementById('player1').value;
+		var p2value = document.getElementById('player2').value;
+	}
+	$.get( 
+		"php/pong.php",
+		{ function: "getOptionsFromLeaderboard" },
+		function(data) {
+			document.getElementById('player1').innerHTML = data;
+			document.getElementById('player2').innerHTML = data;
+			if(playerChanged == 0) {
+				document.getElementById('player2').selectedIndex = 1;
+			}
+			else {
+				$('#player1').val(p1value);
+				$('#player2').val(p2value);
+			}
+			disableDoubles();
+			populateWinnerSelect();
+		}
+	);
+	
+}
+
 function populateWinnerSelect() {
-	console.log('popwin');
+	// on any call other than first, save the selection
+	var selectedIndex = 0;
+	if (document.getElementById('winner').length > 0) {
+		selectedIndex = document.getElementById('winner').selectedIndex;
+	}
 	var p1 = document.getElementById('player1').value;
 	var p2 = document.getElementById('player2').value;
-	var inner = '<option value=\''+player1+'\'>'+player1+'</option><option value=\''+player2+'\'>'+player2+'</option>';
+	var inner = '<option value=\''+p1+'\'>'+p1+'</option><option value=\''+p2+'\'>'+p2+'</option>';
 	document.getElementById('winner').innerHTML = inner;
-	document.getElementById('winner').value = player1;
+	document.getElementById('winner').selectedIndex = selectedIndex;
 }
 
 function disableDoubles() {
-	var p1select = document.getElementById('player1');
-	var p2select = document.getElementById('player2');
-	var p1options = p1select.options;
-	var p2options = p2select.options;
-	var p1value = p1select.value;
-	var p2value = p2select.value;
-	for (int i = 0 ; i < p1options.length ; i++) {
-		if (p1options[i] === p2value){
-			p1select.remove(i);
-			break;
+	var p1value = document.getElementById('player1').value;
+	var p2value = document.getElementById('player2').value;
+	$("#player1 option[value="+p2value+"]").remove();
+	$("#player2 option[value="+p1value+"]").remove();
+}
+
+function getLeaderboard() {
+	$.get( 
+		"php/pong.php",
+		{ function: "getLeaderboard" },
+		function(data) {
+			document.getElementById('leaderboard').innerHTML = data;
 		}
-	}
-	for (int j = 0 ; j < p2options.length ; j++) {
-		if (p2options[j] === p1value){
-			p2select.remove(j);
-			break;
+	);
+}
+
+function logGame(form) {
+	$.get( 
+		"php/pong.php",
+		{ function: "logGame", player1: form.player1.value, player2: form.player2.value, winner: form.winner.value },
+		function(data) {
+			location.reload();
 		}
+	);
+}
+
+function addPlayer() {
+	var player = document.getElementById('newPlayer').value;
+	if (player.length == 0) {
+		document.getElementById('warningLabel').innerHTML = "Needs to have length more than NOTHING";
 	}
+	if ( /[^A-Za-z\d]/.test(player) ) {
+		document.getElementById('warningLabel').innerHTML = "Invalid Characters Detected";
+	}
+	// $.get( 
+// 		"php/pong.php",
+// 		{ function: "addPlayer", player1: player },
+// 		function(data) {
+// 			location.reload();
+// 		}
+// 	);
 }
 
 function startup() {
-	document.getElementById('player2').selectedIndex = 1;
-	disableDoubles();
-	populateWinnerSelect();
+	populatePlayerSelect(0);
+	getLeaderboard();
 }
