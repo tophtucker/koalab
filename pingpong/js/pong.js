6e5a5a67c28b825dbf710915f7a6fdcaf25b26d8
samgqroberts <samgqroberts@gmail.com>
2013-05-30 17:09:47 -0400

    got pingpong back... did I just commit something?

---
+++ pingpong/js/pong.js
@@ -0,0 +1,35 @@
+function populateWinnerSelect() {
+	console.log('popwin');
+	var p1 = document.getElementById('player1').value;
+	var p2 = document.getElementById('player2').value;
+	var inner = '<option value=\''+player1+'\'>'+player1+'</option><option value=\''+player2+'\'>'+player2+'</option>';
+	document.getElementById('winner').innerHTML = inner;
+	document.getElementById('winner').value = player1;
+}
+
+function disableDoubles() {
+	var p1select = document.getElementById('player1');
+	var p2select = document.getElementById('player2');
+	var p1options = p1select.options;
+	var p2options = p2select.options;
+	var p1value = p1select.value;
+	var p2value = p2select.value;
+	for (int i = 0 ; i < p1options.length ; i++) {
+		if (p1options[i] === p2value){
+			p1select.remove(i);
+			break;
+		}
+	}
+	for (int j = 0 ; j < p2options.length ; j++) {
+		if (p2options[j] === p1value){
+			p2select.remove(j);
+			break;
+		}
+	}
+}
+
+function startup() {
+	document.getElementById('player2').selectedIndex = 1;
+	disableDoubles();
+	populateWinnerSelect();
+}
--- pingpong/php/pong.php
+++ pingpong/php/pong.php
@@ -1,121 +1,130 @@
-<?php
-
-include "ChromePhp.php";
-
-if( $_REQUEST["function"] )
-{
-	$function = $_REQUEST['function'];
-	switch($function){
-		case 'getOptionsFromLeaderboard':
-		echo getOptionsFromLeaderboard();
-		break;
-		case 'getLeaderboard':
-		echo getLeaderboard();
-		break;
-		case 'logGame':
-		$player1 = $_REQUEST['player1'];
-		$player2 = $_REQUEST['player2'];
-		$winner = $_REQUEST['winner'];
-		logGame($player1, $player2, $winner);
-		break;
-		case 'addPlayer':
-		$player = $_REQUEST['player1'];
-		addPlayer($player);
-		break;
-		default:
-		return;
-	}
-}
-
-function getOptionsFromLeaderboard() {
-	$file_handle = fopen("./leaderboard.dat", "r");
-	$return_string = "";
-	while (!feof($file_handle)) {
-		$player = fgets($file_handle);
-		$return_string .= "<option value=".$player.">".$player."</option>";
-	}
-	fclose($file_handle);
-	return $return_string;
-}
-
-function getLeaderboard() {
-	$file_handle_leaderboard = fopen("./leaderboard.dat", "r");
-	$return_string = "";
-	$file_handle_players = fopen("./players.dat", "r");
-	$players_array = array();
-	while (!feof($file_handle_players)) {
-		$line = fgets($file_handle_players);
-		if ( $line[0] == "%" ) {
-			// strip the %'s and newline stuff
-			$line = trim($line, "%\n\t");
-			// split into categories
-			$pieces = explode("$", $line);
-			$rest = $pieces[1]."$".$pieces[2]."$".$pieces[3];
-			$players_array[$pieces[0]] = $rest;
-		}
-	}
-	fclose($file_handle_players);
-	$counter = 1;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Koa Labs</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Koa is a shared workspace in the heart of Harvard Square for promising start-ups. Founded in 2012 by serial entrepreneur Andy Palmer, the space provides a collaborative environment to power the next generation of innovation.">
 	
-	//start returnString
-	$return_string = "<h2>Current Leaderboard</h2><br><label class='lbplayer'>Player</label><label class='lbwins'>Wins</label><label class='lblosses'>Losses</label><label class='lblastplayed'>Last Played</label>";
+	<!-- favicon -->
+	<link rel="shortcut icon" href="img/favicon.ico">
 	
-	while (!feof($file_handle_leaderboard)) {
-		$player = trim(fgets($file_handle_leaderboard), " \t\n\r\0");
-		$player_stats = explode("$",$players_array[$player]);
-		$this_entry = "<label class='lbplayer'>".$counter.". ".$player."</label><label class='lbwins'>".$player_stats[0]."</label><label class='lblosses'>".$player_stats[1]."</label><label class='lblastplayed'>".$player_stats[2]."</label>";
-		$return_string .= $this_entry;
-		$counter += 1;
-	}
-	fclose($file_handle_leaderboard);
-	return $return_string;
-}
+	<!-- Styles -->
+	<link href="../css/bootstrap.css" rel="stylesheet">
+	<link href="../css/application.css" rel="stylesheet"> <!-- custom koa style -->
+	<link href="../css/index.css" rel="stylesheet">  <!-- custom koa style -->
+	<link href="pong.css" rel="stylesheet">
+	<link href="../css/bootstrap-responsive.css" rel="stylesheet">
 
-function logGame($player1, $player2, $winner) {
-	$player_file = "players.dat";
-	$file_contents = file_get_contents($player_file);
-	$file_contents = explode("\n", $file_contents);
-	$new_file_contents = "";
-	foreach ($file_contents as $line) {
-		if ($line[0] != "%") {
-			$new_file_contents .= $line."\n";
-			continue;
-		}
-		$line = trim($line, "%\n\t");
-		$line = explode("$", $line);
-		if ($line[0] == $player1) {
-			if ($winner == $player1) {
-				$line[1] = intval($line[1])+1;
-			}
-			else {
-				$line[2] = intval($line[2])+1;
-			}
-			date_default_timezone_set('America/New_York');
-			$line[3] = date('m/d/Y', time());
-		}
-		elseif ($line[0] == $player2) {
-			if ($winner == $player2) {
-				$line[1] = intval($line[1])+1;
-			}
-			else {
-				$line[2] = intval($line[2])+1;
-			}
-			date_default_timezone_set('America/New_York');
-			$line[3] = date('m/d/Y', time());
-		}
-		$line = join("$",$line);
-		$line = "%".$line."%";
-		$new_file_contents .= $line."\n";
-	}
-	$new_file_contents = trim($new_file_contents, "\n");
-	file_put_contents($player_file, $new_file_contents);
-}
+	<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+		<script src="../assets/js/html5shiv.js"></script>
+		<![endif]-->
 
-function addPlayer($player) {
-	//update player.dat
-	$filename = "players.dat";
-	// $file_handle = fopen($filename, "w");
-// 	fwrite($file_handle, "")
-}
+		<!-- Fav and touch icons -->
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+		<link rel="shortcut icon" href="../assets/ico/favicon.png">
+	</head>
+	<body>
+		<div class="totality">
+			<br>
+			<h1>Ping pong is serious business.</h1>
+			<br>
+			
+			<div id="stylized" class="myform">
+				<form id="form" name="form" method="post" action="log_game.php">
+					<h1 align="center">Log a Game</h1>
 
-?>
+					<label>Player 1
+						<span class="small">Select a Player</span>
+					</label>
+					<select name="player1" id="player1"><?php
+						$file_handle = fopen("./leaderboard.dat", "r");
+						while (!feof($file_handle)) {
+						   $line = fgets($file_handle);
+						   echo "<option value=".$line.">".$line."</option>";
+						}
+						fclose($file_handle);
+						?>
+					</select>
+					<br>
+					<br>
+					<br>
+					<label>Player 2
+						<span class="small">Select another Player</span>
+					</label>
+					<select name="player1" id="player1"><?php
+						$file_handle = fopen("./leaderboard.dat", "r");
+						while (!feof($file_handle)) {
+						   $line = fgets($file_handle);
+						   echo "<option value=".$line.">".$line."</option>";
+						}
+						fclose($file_handle);
+						?>
+					</select>
+					
+					<br>
+					<br>
+					<br>
+
+					<label>Phone Number
+						<span class="small">Add a convenient number</span>
+					</label>
+					<input type="tel" name="phone" id="phone" />
+	  
+					<label>Membership Option
+						<span class="small">Select your desired plan</span>
+					</label>
+					<select name="option" id="membership-option">
+						<option value="dedicated-desk">Dedicated Desk</option>
+						<option value="dedicated-office">Dedicated Office</option>
+					</select>
+	  
+					<label>Start Date
+						<span class="small">When do you need the space?</span>
+					</label>
+					<input type="date" name="start-date" id="start-date" />
+	  
+					<label>Number of People
+						<span class="small">How many people need space?</span>
+					</label>
+					<input type="number" name="num-people" id="num-people" min="1" value="1" />
+	  
+					<div class="center">
+						<button type="submit">Apply</button>
+					</div>
+					<div class="spacer"></div>
+
+				</form>
+			</div>
+		
+			<br>
+			<p>Register to play and you start at the bottom of the rankings. You can officially challenge someone who is two ranks above you or less. If you beat them- you get there place and they go down a rank. for example:<br>
+				<br>
+				1. Red <br>
+				2. Blue <br>
+				3. Green <br>
+				4. Yellow <br>
+				<br>
+				Yellow can challenge green or blue. If yellow beats green-<br>
+				<br>
+				1. Red <br>
+				2. Blue <br>
+				3. Yellow <br>
+				4. Green <br>
+				<br>
+				But if yellow challenged blue and won <br>
+				<br>
+				1. Red <br>
+				2. Yellow <br>
+				3. Blue <br>
+				4. Green <br>
+				<br>
+				Notable additions:<br>
+				-you can only officially challenge somebody once per day<br></p>
+			</div>
+		</body>
+		</html>
